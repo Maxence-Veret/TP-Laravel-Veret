e7f2467bd2302847a57328c5222cbd16cd7c7841
Maxence Veret <m.veret62@gmail.com>
2022-02-28 08:59:39 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TP</title>
+</head>
+<body>
+    <h1>Welcome<h1>
+    
+</body>
+</html>
